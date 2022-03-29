9a6712a1ffeeaacd5301c7d1655d3e35d729579d
Corey Steimel <coreysteimel@Coreys-MBP-Work.hsd1.co.comcast.net>
2022-03-29 09:32:35 -0600

    modified?

--- main.js
+++ main.js
@@ -32,50 +32,29 @@ for (let i = 0; i < listItems2.length; i++){
     })
 }
 
-// Having a hard time with bubbling
+// // Having a hard time with bubbling
 // let fadeList = document.querySelector('#fade-list');
 // fadeList.addEventListener('click', function(event){
-//     console.log(event.target.style.opacity)
-
-    // event.target.style.transition = 'opacity 2s';
-
-    // setInterval(function(){
-    //     event.target.style.opacity += '-0.01'
-    // }, 20);
+//     setInterval(function(){
     
-    // Why doesn't this work?? -- reset the setInterval function
-    // if (event.target.style.opacity > '1'){
-    //     setInterval(function(){
-    //         event.target.style.opacity = '1'
-    //     }, 20);
-    //     event.target.style.opacity = '1'
-    // } else if (event.target.style.obacity === '1'){ 
-    //     console.log('working?')
-    //     setInterval(function(){
-    //         event.target.style.opacity = '0'
-    //     }, 20);
-    // } else {
-    //     console.log('working?')
-    //     setInterval(function(){
-    //         event.target.style.opacity += '-0.01'
-    //     }, 20);
-    //     event.target.style.opacity = '0'
-    // }   
+//         if (event.target.style.opacity === '0'){
+//             event.target.style.opacity = '1'
+//         } else {
+//             event.target.style.opacity += '-0.01'
+//         }   
 // });
 
-// now having a hard time with query select all
+// Now Working   :)
 // let row = document.querySelectorAll('#row img');
-
+// console.log(row);
 // for (let i = 0; i < row.length; i++){
-//     img = row[i];
-//     img.style.border = 'solid red'
+//     let img = row[i];
 //     img.addEventListener('click', function(){
-//         console.log(img)
-//         console.log('inside event listener')
+//         img.style.width = '0px'
 //     })
 // }
 
-//trying bubbling
+// trying bubbling
 let row = document.querySelector('#row');
 
 row.addEventListener('click', function(event){
